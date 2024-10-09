9e27bc5075400542a15f3b1cecf981be5c187e4f
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-10-09 23:36:40 +0530

    fonts adjusted

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -8,11 +8,11 @@ const About = () => {
       <h1 className="font-mainheading my-5 md:my-10 text-2xl md:text-3xl lg:text-4xl">
         ABOUT MEHBOBA
       </h1>
-      <h1 className="font-pacifico mt-0 mb-5 p-0 leading-none text-4xl md:text-[680%]">
+      <h1 className="font-pacifico mt-0 mb-5 p-0 text-center leading-none text-4xl md:text-[600%]">
         Welcome to Mehboba.
       </h1>
       <div className="flex flex-col md:flex-row items-center md:items-start justify-center">
-        <h1 className="font-mainheading mt-0 mb-5 p-0 md:pr-5 leading-none text-3xl md:text-[680%] text-center md:text-left">
+        <h1 className="font-mainheading mt-0 mb-5 p-0 md:pr-5 leading-none text-3xl md:text-[600%] text-center md:text-left">
           Celebrate Every
         </h1>
         <div className="w-full md:w-[200px] text-center md:flex flex-wrap justify-center hidden md:mt-5">
@@ -20,12 +20,12 @@ const About = () => {
             Blended with passion, Crafted for you.
           </h1>
         </div>
-        <h1 className="font-mainheading mt-0 mb-5 p-0 md:pl-5 leading-none text-3xl md:text-[680%] text-center md:text-left">
+        <h1 className="font-mainheading mt-0 mb-5 p-0 md:pl-5 leading-none text-3xl md:text-[600%] text-center md:text-left">
           Sip of Our
         </h1>
       </div>
       <div className="flex flex-col md:flex-row items-center md:items-start justify-center">
-        <h1 className="font-mainheading mt-0 mb-10 p-0 md:px-5 leading-none text-3xl md:text-[680%] text-center md:text-left">
+        <h1 className="font-mainheading mt-0 mb-10 p-0 md:px-5 leading-none text-2xl md:text-[600%] text-center md:text-left">
           <span className="font-pacifico overflow-visible pr-2">Delightful</span> Boba
         </h1>
         <div className="w-full md:w-[200px] text-center hidden md:flex flex-wrap justify-center mt-8">
@@ -35,7 +35,7 @@ const About = () => {
         </div>
       </div>
       <div className="items-center justify-center p-4 md:p-8 w-full md:w-4/5">
-        <p className="text-base md:text-lg text-justify leading-relaxed font-content">
+        <p className="text-sm md:text-lg text-justify leading-relaxed font-content">
           At <span className="font-mainheading">Mehboba</span>, we believe in the power of shared experiences and the joy of coming together. 💖 Our boba teas are expertly crafted from the finest ingredients,
           bringing together a delightful fusion of flavors that tell a story with every sip you take. 🥤 Whether you're enjoying a moment with friends or treating yourself to a well-deserved break, our drinks are
           designed to elevate your day and create unforgettable memories. 🌟 Join us on this flavorful journey and savor the sweet moments that life has to offer. ✨
