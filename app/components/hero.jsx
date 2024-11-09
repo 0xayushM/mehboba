919559fe5f2965b1cbd05c943d2cd2f5c4ae7f5e
Ayush Mangal <mangal.ayush.4982@gmail.com>
2024-11-09 12:44:22 +0530

    updated hero section

--- app/components/hero.jsx
+++ app/components/hero.jsx
@@ -1,35 +1,41 @@
 "use client";
 
 import React from 'react';
-import BoxReveal from './boxReveal';
+// import BoxReveal from './boxReveal';
+import PackageModel from './packageModel';
 
 const Hero = () => {
   return (
     <section className="h-screen md:gradient-background">
-      <div className="h-full p-5 z-10 flex flex-col justify-center items-center gap-0">
-        <BoxReveal delay={0.1}>
+      <div className='grid grid-cols-2 h-full'>
+        <div>
+          <PackageModel/>
+        </div>
+      <div className="z-10 flex flex-col justify-center items-center gap-0">
+        {/* <BoxReveal delay={0.1}> */}
           <h1
             color="#FFE8FE"
-            className="font-mainheading text-secondaryBg text-[4rem] md:text-[5rem] lg:text-[9rem] leading-none"
+            className="font-mainheading text-secondaryBg text-[4rem] md:text-[5rem] lg:text-[7rem] leading-none"
           >
             Bursting with <span className='font-pacifico flavour-gradient'>Flavour,</span>
           </h1>
-        </BoxReveal>
-        <BoxReveal delay={0.5}>
+        {/* </BoxReveal>
+        <BoxReveal delay={0.5}> */}
           <h1
             color="#FFE8FE"
-            className="font-mainheading text-[#5523a0] text-[4rem] md:text-[5rem] lg:text-[9rem] leading-none"
+            className="font-mainheading text-[#5523a0] text-[4rem] md:text-[5rem] lg:text-[7rem] leading-none"
           >
             Crafted with <span className='font-pacifico p-2 love-gradient'>Love</span>
           </h1>
-        </BoxReveal>
-        <BoxReveal delay={0.9}>
-          <div color="#FFE8FE" className='flex justify-center items-center'>
-          <p className="font-content text-justify md:w-1/2 font-light text-sm md:text-xl pt-5">
-            Discover premium boba balls that transform every sip into a delightful experience. At <span className='font-mainheading'>MEHBOBA</span>, we take pride in crafting each boba ball with precision, ensuring the perfect balance of texture and flavor in every bite. Whether you're enjoying a refreshing milk tea, a fruity bubble drink, or a creative beverage, our boba balls elevate your drink to an extraordinary level.
+        {/* </BoxReveal>
+        <BoxReveal delay={0.9}> */}
+          <div color="#FFE8FE" className='flex'>
+          <p className="font-content text-justify w-[80%] font-light text-sm md:text-lg pt-5">
+          Discover premium boba balls that transform every sip. At <span className='font-mainheading'>MEHBOBA</span>, we craft each boba ball with precision, ensuring the perfect balance of texture and flavor. Add them to any fruity or creative beverage for a truly exceptional experience.
           </p>
           </div>
-        </BoxReveal>
+        {/* </BoxReveal> */}
+        </div>
       </div>
     </section>
   );
